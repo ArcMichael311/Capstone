c1550ef19b4ae682d66e8e7f7c2811b1cf4b5d3e
ArcMichael311 <michaelraymund.cambal@cit.edu>
2026-08-28 11:59:31 +0800

    fixing router Think there is error

--- Web/phonexis/src/index.js
+++ Web/phonexis/src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Routing from './router/Routing';
-import { AuthProvider } from './context/AuthContext';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <Routing>
-        <App />
-      </Routing>
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
