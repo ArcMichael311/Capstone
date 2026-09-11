7ac7f2439fa62136a0b34814e49ebc069265530e
JNFranzaDin <jonnicole.din@cit.edu>
2026-09-11 14:41:57 +0800

    Added Teacher Activity Repository

---
+++ backend/src/main/java/com/phonexis/backend/Repository/TeacherActivityRepository.java
@@ -0,0 +1,12 @@
+package com.phonexis.backend.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.phonexis.backend.Entity.TeacherActivity;
+import com.phonexis.backend.Entity.User;
+
+public interface TeacherActivityRepository extends JpaRepository<TeacherActivity, Long> {
+	List<TeacherActivity> findByTeacherOrderByCreatedAtDesc(User teacher);
+}
